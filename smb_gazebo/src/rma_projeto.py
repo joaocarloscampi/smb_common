6c4d986db09d809349caaf081ee3b3af10d5a07e
João Carlos Tonon Campi <joaocarloscampi@hotmail.com>
2022-03-22 00:36:43 -0300

    Adicionando csv + parametro launch + processamento separado

--- smb_gazebo/src/rma_projeto.py
+++ smb_gazebo/src/rma_projeto.py
@@ -13,6 +13,7 @@ from geometry_msgs.msg import PoseWithCovarianceStamped
 # Importação de bibliotecas relacionadas
 import numpy as np
 import cv2
+import pandas as pd
 
 class Path:
     def __init__(self):
@@ -114,33 +115,6 @@ def dist_and_angle(x1,y1,x2,y2):
 
 # Função responsável pelo callback de quando recebe uma mensagem LaserScan
 def callbackScan(data):
-    # index, min = minimun(data)
-    #
-    # # Resolução de leitura pelo sensor
-    # angle_increment = data.angle_increment
-    # # De qual angulo o sensor inicia a leitura
-    # angle_min = data.angle_min
-    #
-    # # Angulo onde ocorre a menor distância lida
-    # delta_angle = index*angle_increment
-    # # Angulo com relação ao sensor que ocorre a leitura da menor distância
-    # angle_pillar = delta_angle + angle_min
-    #
-    # # Posição (x, y, z) desse ponto mínimo com respeito ao sensor
-    # xPos_pillar_sensor = min*np.cos(angle_pillar)
-    # yPos_pillar_sensor = min*np.sin(angle_pillar)
-    # zPos_pillar_sensor = 0
-    #
-    # # Obter a transformação do sensor para o base_link do robô
-    # trans, rot = get_tf('rslidar', 'base_link')
-    #
-    # # Transformação de sistemas de coordenadas, do sensor para o robô
-    # xPos_pillar_robot = xPos_pillar_sensor - trans[0]
-    # yPos_pillar_robot = yPos_pillar_sensor - trans[1]
-    # zPos_pillar_robot = zPos_pillar_sensor - trans[2]
-
-    #print(data.pose.pose.position.x)
-
 
     '''xPos = spawnX + data.pose.pose.position.x
     yPos = spawnY + data.pose.pose.position.y
@@ -168,43 +142,6 @@ def callbackScan(data):
     orientation_list = [data.pose.pose.orientation.x, data.pose.pose.orientation.y, data.pose.pose.orientation.z, data.pose.pose.orientation.w]
 
     (roll, pitch, yaw) = euler_from_quaternion (orientation_list)
-
-    '''# if xPos > targetXPos:
-    #     angle_1 = np.arctan2(np.sin(angle-yaw+180), np.cos(angle-yaw+180))
-    # else:
-    angle_1 = np.arctan2(np.sin(angle-yaw), np.cos(angle-yaw))
-
-    print("angleZ: ", np.rad2deg(yaw))
-    print("angle_1: ", np.rad2deg(angle_1))
-
-    sin_angle_1 = np.sin(angle_1)
-
-    print("sin angle_1: ", sin_angle_1)
-
-    # Obtenção do ganho proporcional no parameter server
-    #Kp = rospy.get_param("/smb_highlevel_controller/kp")
-    Kp = 1
-
-    # Equação do controlador P
-    error = Kp*sin_angle_1
-
-    # Publisher para a velocidade do robô após o controlador
-
-    print("distancia: ", objectPath.distPointPath(xPos, yPos))
-    if objectPath.distPointPath(xPos, yPos) < 0.2:
-        #publisherVel([0, 0, 0], [0, 0, 0])
-        if objectPath.indexPath+1 >= len(pathGazebo):
-             publisherVel([0.0, 0, 0], [0, 0, 0])
-        else:
-            objectPath.indexPath = objectPath.indexPath + 1
-    else:
-        if abs(angle_1) > np.deg2rad(5):
-            if error > 0:
-                publisherVel([-0.3, 0, 0], [0, 0, 1]) # apenas 0.2
-            else:
-                publisherVel([-0.3, 0, 0], [0, 0, -1]) # apenas -0.2
-        else:
-            publisherVel([0.3, 0, 0], [0, 0, 0])'''
 
     dist,ang = dist_and_angle(xPos,yPos,targetXPos,targetYPos)
 
@@ -240,7 +177,6 @@ def callbackScan(data):
 
 
 def listener():
-    rospy.init_node('listener', anonymous=True)
 
     #topic_name = rospy.get_param("/smb_highlevel_controller/topic_name")
     #topic_queue_size = rospy.get_param("/smb_highlevel_controller/topic_queue_size")
@@ -254,245 +190,23 @@ def listener():
 
 
 if __name__ == '__main__':
-    '''
-    imgGray = cv2.imread("map.png")
+    rospy.init_node('listener', anonymous=True)
 
-    imgGray = cv2.cvtColor(imgGray, cv2.COLOR_BGR2GRAY)
-
-    imgGray = cv2.resize(imgGray, (0, 0), fx = 0.5, fy = 0.5)
-
-    # cv2.imshow("Segmentada", imgGray)
-    # cv2.waitKey(0)
-
-    (l1, l2) = imgGray.shape[:2]
-
-    costmap = np.zeros((l1,l2))
-
-    xMap = l2
-    yMap = l1
-
-    for i in range(l1):
-        for j in range(l2):
-            if imgGray[i][j] == 255:
-                costmap[i][j] = 0
-            else:
-                costmap[i][j] = -1
-
-    #print(costmap)
-
-    # ------------------------------------------------
-
-    # l2r = l2//4 + 1
-    # l1r = l1r//4 + 1
-    #
-    # imagem = np.ones((l1r, l2r))*(-1)
-    #
-    # for i in range(0, l1, 4): # linha
-    #     for j in range(0, l2, 4): # coluna
-    #
-    #         obstaculo = False
-    #         for ii in range(i, i+4):
-    #             for jj in range(j, j+4):
-    #                 if costmap[jj][ii] == -1:
-    #                     obstaculo = True
-    #         if obstaculo:
-    #             imagem no pixel = -1
-
-    # --------------------------------------------------
-
-    # padrão: [x, y, d]
-
-    iteracao = 0
-
-    # Define inicio
-    # xStart = 50
-    # yStart = 500
-
-    xStart = 120
-    yStart = 230
-
-    # Define final
-    # xEnd = 120
-    # yEnd = 500
-
-    xEnd = 60
-    yEnd = 60
-
-    # Cria vetor nó
-    nodes = []
-
-    # Adiciona final como primeiro no
-    nodes = addNode(nodes, xEnd, yEnd, 1)
-
-    print("Gerando o mapa... (Isso pode demorar um pouco)")
-
-    # while que vai até acabar os nó
-    while(len(nodes) > 0):
-        #print("-----------Iteracao ", iteracao)
-        # lista auxiliar pra armazenar novos nós
-        new_nodes = []
-
-        # verificar cada vizinho em cada no da onda
-        for node in nodes:
-            xNode = node[0]
-            yNode = node[1]
-            dNode = node[2]
-
-            costmap[yNode][xNode] = dNode
-
-            # verifica leste
-            if ((xNode + 1) < xMap and costmap[yNode][xNode+1] == 0):
-                new_nodes = addNode(new_nodes, xNode+1, yNode, dNode+1)
-
-            # verifica oeste
-            if ((xNode - 1) >= 0 and costmap[yNode][xNode-1] == 0):
-                new_nodes = addNode(new_nodes, xNode-1, yNode, dNode+1)
-
-            # verifica norte
-            if ((yNode - 1) >= 0 and costmap[yNode-1][xNode] == 0):
-                new_nodes = addNode(new_nodes, xNode, yNode-1, dNode+1)
-
-            # verifica norte
-            if ((yNode + 1) < yMap and costmap[yNode+1][xNode] == 0):
-                new_nodes = addNode(new_nodes, xNode, yNode+1, dNode+1)
-
-            for i in range (len(new_nodes)):
-                for j in range(i+1, len(new_nodes)):
-                    if p(new_nodes[i]) > p(new_nodes[j]):
-                        new_nodes[i], new_nodes[j] = new_nodes[j], new_nodes[i]
-            #print(new_nodes)
-
-
-            index = 0
-
-            while(index < len(new_nodes)-1):
-                if p(new_nodes[index]) == p(new_nodes[index+1]):
-                    new_nodes.pop(index+1)
-                else:
-                    index = index + 1
-
-            #print(costmap)
-            iteracao = iteracao + 1
-
-            nodes = []
-            nodes = new_nodes
-
-    path = []
-
-    path.append([xStart, yStart])
-
-    nLocX = xStart
-    nLocY = yStart
-
-    bNoPath = False
-
-    while( not (nLocX == xEnd and nLocY == yEnd) and not bNoPath):
-
-        listVizinhos = []
-
-        #print("Ponto: " +  str(nLocX) + " , " + str(nLocY))
-        # 4-Way Connectivity
-        # verifica norte
-        if ((nLocY - 1) >= 0 and costmap[nLocY - 1][nLocX] > 0):
-            listVizinhos = addNode(listVizinhos, nLocX, nLocY-1, costmap[nLocY-1][nLocX])
-
-        # verifica sul
-        if ((nLocY + 1) < yMap and costmap[nLocY + 1][nLocX] > 0):
-            listVizinhos = addNode(listVizinhos, nLocX, nLocY+1, costmap[nLocY+1][nLocX])
-
-        # verifica leste
-        if ((nLocX + 1) < xMap and costmap[nLocY][nLocX+1] > 0):
-            listVizinhos = addNode(listVizinhos, nLocX+1, nLocY, costmap[nLocY][nLocX+1])
-
-        # verifica oeste
-        if ((nLocX - 1) >= 0 and costmap[nLocY ][nLocX-1] > 0):
-            listVizinhos = addNode(listVizinhos, nLocX-1, nLocY, costmap[nLocY][nLocX-1])
-
-        # CASO QUEIRA TIRAR A VIZINHANÇA 8, COMENTAR EM BAIXO
-
-        #verifica noroeste
-        if ((nLocY -1) >= 0 and (nLocX -1) >= 0 and costmap[nLocY-1][nLocX-1] > 0):
-            listVizinhos = addNode(listVizinhos, nLocX-1, nLocY-1, costmap[nLocY-1][nLocX-1])
-
-        #verifica nordeste
-        if ((nLocY -1) >= 0 and (nLocX +1) < xMap and costmap[nLocY-1][nLocX+1] > 0):
-            listVizinhos = addNode(listVizinhos, nLocX+1, nLocY-1, costmap[nLocY-1][nLocX+1])
-
-        #verifica sudoeste
-        if ((nLocY +1) < yMap and (nLocX -1) >= 0 and costmap[nLocY+1][nLocX-1] > 0):
-            listVizinhos = addNode(listVizinhos, nLocX-1, nLocY+1, costmap[nLocY+1][nLocX-1])
-
-        #verfica sudeste
-        if ((nLocY +1) < yMap and (nLocX +1) < xMap and costmap[nLocY+1][nLocX+1] > 0):
-            listVizinhos = addNode(listVizinhos, nLocX+1, nLocY+1, costmap[nLocY+1][nLocX+1])
-
-        for i in range (len(listVizinhos)):
-            for j in range(i+1, len(listVizinhos)):
-                if listVizinhos[i][2] > listVizinhos[j][2]:
-                    listVizinhos[i], listVizinhos[j] = listVizinhos[j], listVizinhos[i]
-
-        if len(listVizinhos) == 0:
-            bNoPath = True
-        else:
-            nLocX = listVizinhos[0][0]
-            nLocY = listVizinhos[0][1]
-            path.append([nLocX, nLocY]);
-
-
-
-    # for i in range(len(path)):
-    #     print(path[i])
-    #     if imgGray[path[i][1]+10][path[i][0]] == 0:
-    #         print("entrei1")
-    #         newY = path[i][1] - 5
-    #         if imgGray[newY][path[i][0]] != 0:
-    #             print("entrei2")
-    #             path[i][1] = newY
-    #
-    # for i in range(len(path)):
-    #     print(path[i])
-    #     if imgGray[path[i][1]-10][path[i][0]] == 0:
-    #         print("entrei1")
-    #         newY = path[i][1] + 5
-    #         if imgGray[newY][path[i][0]] != 0:
-    #             print("entrei2")
-    #             path[i][1] = newY
-
-
-
-    for i in range(len(path)):
-        x = path[i][0]
-        y = path[i][1]
-        imgGray[y][x] = 150
-
-    print(convertPixelToGazebo(path[0][0], path[0][1]))
-
-
-
-    pathGazebo = []
-
-    for i in range(0, len(path), 5):
-        x = path[i][0]
-        y = path[i][1]
-        imgGray[y][x] = 200
-
-        xPath, yPath = convertPixelToGazebo(path[i][0], path[i][1])
-
-        pathGazebo.append([xPath, yPath])
-
-    print("Caminho gerado. Iniciando movimentação do robô")
-
-    cv2.imwrite("Segmentada", imgGray)
-    #cv2.waitKey(0)
-
-    objectPath.getPath(pathGazebo)
-
-    # '''
+    file_map = rospy.get_param('~path_map')
 
     #pathGazebo = [[13.0, 5.385999999999999], [13.5, 5.385999999999999], [14.0, 4.885999999999999], [14.5, 4.785999999999998], [15.0, 4.785999999999998], [15.5, 4.785999999999998], [16.0, 4.785999999999998], [16.5, 4.785999999999998], [17.0, 4.785999999999998], [17.5, 4.785999999999998], [18.0, 4.785999999999998], [18.5, 4.785999999999998], [19.0, 4.785999999999998], [19.5, 4.785999999999998], [20.0, 4.785999999999998], [20.200000000000003, 3.8859999999999992], [20.200000000000003, 3.3859999999999992], [20.200000000000003, 2.8859999999999992], [20.200000000000003, 2.3859999999999992], [19.900000000000002, 2.0859999999999985], [19.400000000000002, 2.0859999999999985], [18.900000000000002, 2.0859999999999985], [18.400000000000002, 2.0859999999999985], [17.900000000000002, 2.0859999999999985], [17.400000000000002, 2.0859999999999985], [16.9, 2.0859999999999985], [16.4, 2.0859999999999985], [15.9, 2.0859999999999985], [15.4, 2.0859999999999985], [14.9, 2.0859999999999985], [14.4, 2.0859999999999985], [13.9, 2.0859999999999985], [13.4, 2.0859999999999985], [12.9, 2.0859999999999985], [12.4, 2.0859999999999985], [11.9, 2.0859999999999985], [11.4, 2.0859999999999985], [10.9, 2.0859999999999985], [10.4, 2.0859999999999985], [9.9, 2.0859999999999985], [9.700000000000001, 2.5859999999999985], [9.700000000000001, 3.0859999999999985], [9.700000000000001, 3.5859999999999985], [9.700000000000001, 4.0859999999999985], [9.700000000000001, 4.5859999999999985], [9.700000000000001, 5.0859999999999985], [9.700000000000001, 5.5859999999999985], [9.700000000000001, 6.0859999999999985], [9.700000000000001, 6.5859999999999985], [9.700000000000001, 7.0859999999999985], [9.700000000000001, 7.5859999999999985], [9.700000000000001, 8.085999999999999], [10.0, 9.085999999999999], [10.5, 9.085999999999999], [11.0, 9.085999999999999], [11.5, 9.085999999999999], [12.0, 9.686], [12.5, 9.686], [13.0, 10.186], [13.5, 10.686], [14.0, 11.186], [14.5, 11.485999999999997], [15.0, 11.485999999999997], [15.5, 11.485999999999997], [16.0, 11.485999999999997], [16.5, 11.485999999999997], [17.0, 11.485999999999997], [17.5, 11.485999999999997], [18.0, 11.485999999999997], [18.5, 11.485999999999997], [19.0, 11.485999999999997], [19.5, 11.485999999999997], [20.0, 11.485999999999997], [20.5, 11.585999999999999], [21.0, 12.085999999999999], [21.1, 12.585999999999999], [21.1, 12.585999999999999], [21.1, 13.085999999999999], [21.1, 13.585999999999999], [21.1, 14.085999999999999], [21.1, 14.585999999999999], [21.1, 15.085999999999999], [21.1, 15.585999999999999], [21.1, 16.086], [21.1, 16.586], [21.1, 17.086], [21.1, 17.586], [21.1, 18.086], [21.1, 18.586], [21.1, 19.086], [21.1, 19.586], [21.1, 20.086], [21.1, 20.586], [21.1, 21.086], [21.1, 21.586], [21.1, 22.086], [21.0, 22.586], [20.5, 23.186], [20.0, 23.186], [19.5, 23.186], [19.0, 23.186], [18.5, 22.286], [18.0, 21.786], [17.5, 21.286], [17.0, 20.786], [16.5, 20.286], [16.4, 19.786], [16.4, 19.286], [16.4, 19.286], [16.4, 18.785999999999998], [15.9, 18.086], [15.4, 18.086], [14.9, 18.086], [14.4, 18.086], [13.9, 18.086], [13.4, 18.086], [12.9, 18.086], [12.4, 18.086], [11.9, 18.086], [11.4, 18.086], [10.9, 18.086], [10.4, 18.086], [9.9, 18.086], [9.4, 18.886], [8.9, 18.886], [8.4, 19.386], [7.9, 19.886], [7.4, 20.386], [7.0, 20.886], [7.0, 21.386], [7.0, 21.886], [7.0, 22.386]]
 
-    pathGazebo = [[13.0, 6.0], [13.5, 5.5], [14.0, 5.0], [14.5, 4.899999999999999], [15.0, 4.899999999999999], [15.5, 4.899999999999999], [16.0, 4.899999999999999], [16.5, 4.899999999999999], [17.0, 4.899999999999999], [17.5, 4.899999999999999], [18.0, 4.899999999999999], [18.5, 4.899999999999999], [19.0, 4.899999999999999], [19.5, 4.899999999999999], [20.0, 4.899999999999999], [20.200000000000003, 4.5], [20.200000000000003, 4.0], [20.200000000000003, 3.5], [20.200000000000003, 3.0], [19.900000000000002, 2.6999999999999993], [19.400000000000002, 2.6999999999999993], [18.900000000000002, 2.6999999999999993], [18.400000000000002, 2.6999999999999993], [17.900000000000002, 2.6999999999999993], [17.400000000000002, 2.6999999999999993], [16.9, 2.6999999999999993], [16.4, 2.6999999999999993], [15.9, 2.6999999999999993], [15.4, 2.6999999999999993], [14.9, 2.6999999999999993], [14.4, 2.6999999999999993], [13.9, 2.6999999999999993], [13.4, 2.6999999999999993], [12.9, 2.6999999999999993], [12.4, 2.6999999999999993], [11.9, 2.6999999999999993], [11.4, 2.6999999999999993], [10.9, 2.6999999999999993], [10.4, 2.6999999999999993], [9.9, 2.6999999999999993], [9.700000000000001, 3.1999999999999993], [9.700000000000001, 3.6999999999999993], [9.700000000000001, 4.199999999999999], [9.700000000000001, 4.699999999999999], [9.700000000000001, 5.199999999999999], [9.700000000000001, 5.699999999999999], [9.700000000000001, 6.199999999999999], [9.700000000000001, 6.699999999999999], [9.700000000000001, 7.199999999999999], [9.700000000000001, 7.699999999999999], [9.700000000000001, 8.2], [9.700000000000001, 8.7], [10.0, 9.2], [10.5, 9.7], [11.0, 9.7], [11.5, 9.7], [12.0, 9.799999999999997], [12.5, 10.3], [13.0, 10.8], [13.5, 11.3], [14.0, 11.8], [14.5, 12.099999999999998], [15.0, 12.099999999999998], [15.5, 12.099999999999998], [16.0, 12.099999999999998], [16.5, 12.099999999999998], [17.0, 12.099999999999998], [17.5, 12.099999999999998], [18.0, 12.099999999999998], [18.5, 12.099999999999998], [19.0, 12.099999999999998], [19.5, 12.099999999999998], [20.0, 12.099999999999998], [20.5, 12.2], [21.0, 12.2], [21.1, 12.7], [21.1, 13.2], [21.1, 13.7], [21.1, 14.2], [21.1, 14.7], [21.1, 15.2], [21.1, 15.7], [21.1, 16.2], [21.1, 16.7], [21.1, 17.2], [21.1, 17.7], [21.1, 18.2], [21.1, 18.7], [21.1, 19.2], [21.1, 19.7], [21.1, 20.2], [21.1, 20.7], [21.1, 21.2], [21.1, 21.7], [21.1, 22.2], [21.1, 22.7], [21.0, 23.2], [20.5, 23.3], [20.0, 23.3], [19.5, 23.3], [19.0, 23.3], [18.5, 22.9], [18.0, 22.4], [17.5, 21.9], [17.0, 21.4], [16.5, 20.9], [16.4, 20.4], [16.4, 19.9], [16.4, 19.4], [16.4, 18.9], [15.9, 18.7], [15.4, 18.7], [14.9, 18.7], [14.4, 18.7], [13.9, 18.7], [13.4, 18.7], [12.9, 18.7], [12.4, 18.7], [11.9, 18.7], [11.4, 18.7], [10.9, 18.7], [10.4, 18.7], [9.9, 18.7], [9.4, 19.0], [8.9, 19.5], [8.4, 20.0], [7.9, 20.5], [7.4, 21.0], [7.0, 21.5], [7.0, 22.0], [7.0, 22.5], [7.0, 23.0]]
-    
+    #pathGazebo = [[13.0, 6.0], [13.5, 5.5], [14.0, 5.0], [14.5, 4.899999999999999], [15.0, 4.899999999999999], [15.5, 4.899999999999999], [16.0, 4.899999999999999], [16.5, 4.899999999999999], [17.0, 4.899999999999999], [17.5, 4.899999999999999], [18.0, 4.899999999999999], [18.5, 4.899999999999999], [19.0, 4.899999999999999], [19.5, 4.899999999999999], [20.0, 4.899999999999999], [20.200000000000003, 4.5], [20.200000000000003, 4.0], [20.200000000000003, 3.5], [20.200000000000003, 3.0], [19.900000000000002, 2.6999999999999993], [19.400000000000002, 2.6999999999999993], [18.900000000000002, 2.6999999999999993], [18.400000000000002, 2.6999999999999993], [17.900000000000002, 2.6999999999999993], [17.400000000000002, 2.6999999999999993], [16.9, 2.6999999999999993], [16.4, 2.6999999999999993], [15.9, 2.6999999999999993], [15.4, 2.6999999999999993], [14.9, 2.6999999999999993], [14.4, 2.6999999999999993], [13.9, 2.6999999999999993], [13.4, 2.6999999999999993], [12.9, 2.6999999999999993], [12.4, 2.6999999999999993], [11.9, 2.6999999999999993], [11.4, 2.6999999999999993], [10.9, 2.6999999999999993], [10.4, 2.6999999999999993], [9.9, 2.6999999999999993], [9.700000000000001, 3.1999999999999993], [9.700000000000001, 3.6999999999999993], [9.700000000000001, 4.199999999999999], [9.700000000000001, 4.699999999999999], [9.700000000000001, 5.199999999999999], [9.700000000000001, 5.699999999999999], [9.700000000000001, 6.199999999999999], [9.700000000000001, 6.699999999999999], [9.700000000000001, 7.199999999999999], [9.700000000000001, 7.699999999999999], [9.700000000000001, 8.2], [9.700000000000001, 8.7], [10.0, 9.2], [10.5, 9.7], [11.0, 9.7], [11.5, 9.7], [12.0, 9.799999999999997], [12.5, 10.3], [13.0, 10.8], [13.5, 11.3], [14.0, 11.8], [14.5, 12.099999999999998], [15.0, 12.099999999999998], [15.5, 12.099999999999998], [16.0, 12.099999999999998], [16.5, 12.099999999999998], [17.0, 12.099999999999998], [17.5, 12.099999999999998], [18.0, 12.099999999999998], [18.5, 12.099999999999998], [19.0, 12.099999999999998], [19.5, 12.099999999999998], [20.0, 12.099999999999998], [20.5, 12.2], [21.0, 12.2], [21.1, 12.7], [21.1, 13.2], [21.1, 13.7], [21.1, 14.2], [21.1, 14.7], [21.1, 15.2], [21.1, 15.7], [21.1, 16.2], [21.1, 16.7], [21.1, 17.2], [21.1, 17.7], [21.1, 18.2], [21.1, 18.7], [21.1, 19.2], [21.1, 19.7], [21.1, 20.2], [21.1, 20.7], [21.1, 21.2], [21.1, 21.7], [21.1, 22.2], [21.1, 22.7], [21.0, 23.2], [20.5, 23.3], [20.0, 23.3], [19.5, 23.3], [19.0, 23.3], [18.5, 22.9], [18.0, 22.4], [17.5, 21.9], [17.0, 21.4], [16.5, 20.9], [16.4, 20.4], [16.4, 19.9], [16.4, 19.4], [16.4, 18.9], [15.9, 18.7], [15.4, 18.7], [14.9, 18.7], [14.4, 18.7], [13.9, 18.7], [13.4, 18.7], [12.9, 18.7], [12.4, 18.7], [11.9, 18.7], [11.4, 18.7], [10.9, 18.7], [10.4, 18.7], [9.9, 18.7], [9.4, 19.0], [8.9, 19.5], [8.4, 20.0], [7.9, 20.5], [7.4, 21.0], [7.0, 21.5], [7.0, 22.0], [7.0, 22.5], [7.0, 23.0]]
+
+
+    df = pd.read_csv(file_map)
+
+    tamanho = df.size
+
+    pathGazebo = []
+
+    for i in range(int(tamanho/2)):
+        pathGazebo.append([df.iloc[i]['x'], df.iloc[i]['y']])
 
     print(pathGazebo)
     objectPath.getPath(pathGazebo)
